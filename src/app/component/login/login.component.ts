084c8f86a116feb2aa6a865a5edde1580b3eee69
Dave <d.lieu@live.com>
2018-06-28 21:30:42 -0400

    Login validation with spacing + code clean

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
@@ -18,8 +18,8 @@ export class LoginComponent implements OnInit {
               private authService: AuthenticationService,
               private snackBar: MatSnackBar) {
     this.loginForm = this.fb.group({
-      email: '',
-      password: ''
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
