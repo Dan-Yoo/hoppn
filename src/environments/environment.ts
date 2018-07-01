8f24492c52d12ce0702d8793e9f32d3f8c1a2b2f
Dan-Yoo <yoo.d93@gmail.com>
2018-07-01 15:32:52 -0400

    temporary comit to allow untracked files

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBKGgoQFdpEGVk3XSiPPgvXCiLWWgZnMvU",
+        authDomain: "hoppn-dev.firebaseapp.com",
+        databaseURL: "https://hoppn-dev.firebaseio.com",
+        projectId: "hoppn-dev",
+        storageBucket: "hoppn-dev.appspot.com",
+        messagingSenderId: "870165855670"
+    },
+    google: {
+        apiKey: "AIzaSyCQ08Uc38qoTz8d9mm5aC5_ZtsT--xalJ4"
+    }
+};
